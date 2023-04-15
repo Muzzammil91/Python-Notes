83e92dd985a117fd51ad139a2e2a5f552086fd9c
Muzzammil91 <98688013+Muzzammil91@users.noreply.github.com>
2023-04-15 22:06:12 +0500

    Python problem question added

---
+++ Problems/Turbine in array.py
@@ -0,0 +1,12 @@
+# make a 5X5 array with 1 on outside dimensions then 0 on inside dimensions then 91 in Center.
+
+import numpy as np
+
+final_arr = np.ones((5, 5), dtype='int32')
+ones_arr = np.zeros((3, 3),  dtype='int32')
+
+final_arr[1:-1, 1:-1] = ones_arr
+
+final_arr[2, 2] = 7
+
+print(final_arr)
