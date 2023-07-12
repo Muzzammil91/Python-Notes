16b11f199f1cf80b16b5298ba16b21d2658c164a
Muzzammil91 <98688013+Muzzammil91@users.noreply.github.com>
2023-07-12 18:42:38 +0500

    moved problems to commutative repo

--- Problems/input list of numbers comma separated and get sum of them.py
+++
@@ -1,9 +0,0 @@
-# input list of numbers comma separated and get sum of them
-
-x = input("Enter a list of numbers separated by commas :")
-sum = 0
-
-for i in x.split(','):
-    sum = sum + int(i)
-
-print("The sum of the numbers in the list is: ", sum)
